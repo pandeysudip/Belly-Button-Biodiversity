4ab59f4f27689cfa63ad85bf394fde2688337445
pandeysudip <sudip@siu.edu>
2021-11-12 21:56:33 -0800

    test

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
 //loading JSON file using d3.json()
 d3.json("samples.json").then(function (data) {
-    console.log(data.samples);
+    let samples = data.samples;
+    for (let i = 0; i < samples.length; i++) {
+        let otu_id = samples[i].otu_ids;
+        let sample_values = samples[i].sample_values;
+        let otu_labels = samples[i].otu_labels;
+    }
+    console.log(otu_id);
 });
 
 //const dataPromise = d3.json("samples.json");
