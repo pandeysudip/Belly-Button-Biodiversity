d47b10a19d460b1351e2405dd47d531667a2b929
pandeysudip <sudip@siu.edu>
2021-11-12 20:58:57 -0800

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 //loading JSON file using d3.json()
 d3.json("samples.json").then(function (data) {
-    console.log(data);
+    console.log("Data1", data);
 });
 
 const dataPromise = d3.json("samples.json");
-console.log("Data Promise: ", dataPromise);;
+console.log("Data Promise: ", dataPromise);
