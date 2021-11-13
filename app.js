bb4ad514a53e8909d3176f64b119abd87c65672e
pandeysudip <sudip@siu.edu>
2021-11-12 22:08:08 -0800

    test

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 d3.json("samples.json").then(function (data) {
     let samples = data.samples;
     for (let i = 0; i < samples.length; i++) {
-        let otu_id = samples[i].otu_ids;
-        let sample_values = samples[i].sample_values;
-        let otu_labels = samples[i].otu_labels;
+        let id = samples[i].id;
+        let otu_id = samples[i].otu_ids.slice(0, 11);
+        let sample_values = samples[i].sample_values.slice(0, 11);
+        let otu_labels = samples[i].otu_labels.slice(0, 11);
+        console.log(otu_id);
     }
-    console.log(otu_id);
+
 });
 
 //const dataPromise = d3.json("samples.json");
