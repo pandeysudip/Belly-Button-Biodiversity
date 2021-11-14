fe88c005fa3d5f2357cbf0b3118d3e708ff8baaf
pandeysudip <sudip@siu.edu>
2021-11-14 10:45:31 -0800

    plotting meta data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,3 +14,30 @@ function init() {
     })
 };
 
+function graphs(names) {
+    d3.json("samples.json").then((data) => {
+        //metadata variables
+        var metadata = data.metadata;
+        //select demographic table
+        var initTable = d3.select("#sample-metadata");
+
+        //clearing table data
+        initTable.html("");
+
+        //variable for each samples metadata
+        var singleMetadata = metadata.filter(sample => sample.id == names)[0];
+
+        //to add the data to the table
+        Object.entries(singleMetadata).forEach(([key, value]) => {
+            initTable.append("h5").text(`${key}: ${value}`);
+        });
+
+
+    })
+};
+
+function optionChanged(newSample) {
+    graphs(newSample);
+};
+
+init();
